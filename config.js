c21b460fce84705c17df2d1fff9f5551f98856fe
Sudaissz <abubakarmaikasuwa35@gmail.com>
2024-09-01 14:26:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,9 +23,9 @@ global.location = '1'; // Replace with your location panel
 //===========================//
 
 global.prefa = ['', '!', '.', ',', '🐤', '🗿'];
-global.owner = ['2348133729715'];
-global.ownMain = '2348133729715';
-global.NamaOwner = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Do not change 
+global.owner = ['2349019760022'];
+global.ownMain = '2349019760022';
+global.NamaOwner = '𝚩𝐔𝐆'; // Do not change 
 global.sessionName = 'session';
 global.connect = true; // Set to false if using QR Code
 global.namabot = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Do not change
